5d995ed7274788a0af56171285bab4d1ef9f6681
kikedare78@gmail.com <kikedare78@gmail.com>
2024-06-05 16:38:12 +0100

    Created Service class

---
+++ src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryService.java
@@ -0,0 +1,33 @@
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+public class GratitudeEntryService {
+
+public GratitudeEntryRepository gratitudeEntryRepository; 
+
+    public GratitudeEntryService(GratitudeEntryRepository gratitudeEntryRepository) {
+        this.gratitudeEntryRepository = gratitudeEntryRepository; 
+    }
+    public GratitudeEntry createGratitudeEntry(GratitudeEntry newEntry) throws IllegalArgumentException, OptimisticLockingFailureException {
+        return gratitudeEntryRepository.save(gratitudeEntry);
+    }
+
+    public List<GratitudeEntryX> getAllGratitudeEntry(){
+        return gratitudeEntryRepository.findAll();
+    }
+
+    public getGratitudeEntry(UUID entryid) throws NoSuchElementException {
+        return gratitudeEntryRepository.findById(userId).orElseThrow();
+    }
+
+    public GratitudeEntry updateGratitudeEntry(UUID id, GratitudeEntry updatedgraGratitudeEntry) throws NoSuchElementException{
+        GratitudeEntry gratitudeEntry = gratitudeEntryRepository.findById(id).orElseThrow();
+        gratitudeEntry.setContent(updatedgraGratitudeEntry.getContent());
+    }
+
+
+}
+   
+
+

8510a1c2a1f1d6a8a6289731eeb5621adfe4e874
kikedare78@gmail.com <kikedare78@gmail.com>
2024-06-12 18:22:48 +0100

    Updated Gratitude entry service file with Update and Delete method

--- src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryService.java
+++ src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryService.java
@@ -9,27 +9,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class GratitudeEntryService {
 
-public GratitudeEntryRepository gratitudeEntryRepository; 
+public GratitudeEntryRepository gratitudeEntryRepository;   // Instance of Repository class
 
     public GratitudeEntryService(GratitudeEntryRepository gratitudeEntryRepository) {
         this.gratitudeEntryRepository = gratitudeEntryRepository; 
     }
     public GratitudeEntry createGratitudeEntry(GratitudeEntry createdGratitudeEntry) throws IllegalArgumentException, OptimisticLockingFailureException {
-        return gratitudeEntryRepository.save(createdGratitudeEntry);
+        return gratitudeEntryRepository.save(createdGratitudeEntry);    // createed GratitudeEntry
     }
 
-    public List<GratitudeEntry> getAllGratitudeEntry(){
+    public List<GratitudeEntry> getAllGratitudeEntry(){     // retrive all entries
         return gratitudeEntryRepository.findAll();
     }
 
     public GratitudeEntry getGratitudeEntry(UUID entryId) throws NoSuchElementException {
         return gratitudeEntryRepository.findById(entryId).orElseThrow();
     }
-
-    public GratitudeEntry updateGratitudeEntry(UUID id, GratitudeEntry updatedgraGratitudeEntry) throws NoSuchElementException{
-        GratitudeEntry gratitudeEntry = gratitudeEntryRepository.findById(id).orElseThrow();
+        // To update an entry
+    public GratitudeEntry updateGratitudeEntry(UUID entryId, GratitudeEntry updatedgraGratitudeEntry) throws NoSuchElementException{
+        GratitudeEntry gratitudeEntry = gratitudeEntryRepository.findById(entryId).orElseThrow();
         gratitudeEntry.setContent(updatedgraGratitudeEntry.getContent());
+        gratitudeEntry.setUserName(updatedgraGratitudeEntry.getUserName());
+        gratitudeEntry.setUpdated(updatedgraGratitudeEntry.getUpdated());
+        gratitudeEntry.setTopic(updatedgraGratitudeEntry.getTopic());
+        gratitudeEntry.setLocation(updatedgraGratitudeEntry.getLocation());
+        
         return gratitudeEntry;
+    }
+
+    public void deleteGratitudeEntry(UUID entryId) { // To delete an entry by id
+        gratitudeEntryRepository.deleteById(entryId);
     }
 
 

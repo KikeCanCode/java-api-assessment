74ff0c7f0df242a847837d685b38690e33ccaaed
kikedare78@gmail.com <kikedare78@gmail.com>
2024-06-12 18:03:48 +0100

    added Delete end point

--- src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
+++ src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,18 @@ public class GratitudeEntryController {
             }
     }
 
-//Delete
+    
+//Delete - deletes an entry by id
+ @DeleteMapping("/{id}")
+
+    public ResponseEntity<Void> deleteGratitudeEntry(@PathVariable UUID entryId) {
+        try {
+            gratitudeEntryService.getGratitudeEntry(entryId);
+            gratitudeEntryService.deleteGratitudeEntry(entryId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (IllegalArgumentException | NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 }

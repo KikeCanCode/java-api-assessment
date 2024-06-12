9899361711ed11e6ccbecbd7da855fa321ded112
kikedare78@gmail.com <kikedare78@gmail.com>
2024-06-12 12:18:04 +0100

    Created get endpoint for id

--- src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
+++ src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
@@ -1,8 +1,13 @@
 package com.cbfacademy.apiassessment.GratitudeJournalingApi;
 
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +33,17 @@ public class GratitudeEntryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdGratitudeEntry);
     }
 
-// Get 
+// Get GrattitudeEntry by entry id 
+@GetMapping("/{id}") //("/{created}")// Get GratitudeEntry by user id? or something else?
+
+    public ResponseEntity <GratitudeEntry> getGratitudeEntry(@PathVariable UUID id) {
+    //public ResponseEntity <GratitudeEntry> getGratitudeEntry(@PathVariable String created) {
+        try {
+            return new ResponseEntity<>(gratitudeEntryService.getGratitudeEntry(id),HttpStatus.OK);
+        } catch (NoSuchElementException noSuchElementException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
 // Put
 

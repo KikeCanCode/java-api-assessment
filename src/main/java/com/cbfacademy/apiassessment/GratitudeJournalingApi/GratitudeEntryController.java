b82f4dbc2af89efb6b3b497844ac3e042631c72d
kikedare78@gmail.com <kikedare78@gmail.com>
2024-06-15 16:30:32 +0100

    Updated put mapp patch variable id

--- src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
+++ src/main/java/com/cbfacademy/apiassessment/GratitudeJournalingApi/GratitudeEntryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,9 +54,10 @@ public class GratitudeEntryController {
     }
 
         // Put - To update a GratitudeEntry 
-    public ResponseEntity<GratitudeEntry> updateGratitudeEntry(@PathVariable UUID userId, @RequestBody GratitudeEntry updatedGratitudeEntry) {
+        @PutMapping("/{entryId}")
+    public ResponseEntity<GratitudeEntry> updateGratitudeEntry(@PathVariable UUID entryId, @RequestBody GratitudeEntry updatedGratitudeEntry) {
         try {
-            GratitudeEntry updatedgratitudeEntry = gratitudeEntryService.updateGratitudeEntry(userId, updatedGratitudeEntry);
+            GratitudeEntry updatedgratitudeEntry = gratitudeEntryService.updateGratitudeEntry(entryId, updatedGratitudeEntry);
                 return ResponseEntity.status(HttpStatus.RESET_CONTENT).body(updatedgratitudeEntry);    
         } 
         catch (NoSuchElementException noSuchElementException) { // when an element cannot be found.
